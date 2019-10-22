d81e66c80c3681157aa4565c638b28e3187e54d0
Ilya Kuznetsov <et.anchoret@gmail.com>
2019-10-22 22:30:36 +0300

    Parent gradle build and settings files were corrected.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,20 +1,3 @@
-plugins {
-    id("org.jetbrains.kotlin.jvm") version "1.3.41"
-    application
-}
-
-repositories {
-    jcenter()
-}
-
-dependencies {
-    implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-
-    testImplementation("org.jetbrains.kotlin:kotlin-test")
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
-}
-
-application {
-    mainClassName = "com.ran.kolibri.AppKt"
+subprojects {
+    version = "1.0"
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,9 @@
 rootProject.name = "kolibri-backend"
+
+include(
+    "kolibri-model",
+    "kolibri-core",
+    "kolibri-idp:kolibri-idp-core",
+    "kolibri-idp:kolibri-idp-api",
+    "kolibri-idp:kolibri-idp-scheduler"
+)
