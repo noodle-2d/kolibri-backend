82a16c2e2484e71c63b7d3b3cbfae64500e6672f
Ilya Kuznetsov <et.anchoret@gmail.com>
2019-10-22 22:24:23 +0300

    Kolibri core module was added with stub classes.

---
+++ kolibri-core/src/main/kotlin/com/ran/kolibri/core/CoreClass.kt
@@ -0,0 +1,9 @@
+package com.ran.kolibri.core
+
+import com.ran.kolibri.model.ModelClass
+
+class CoreClass {
+    fun someLibraryMethod(): Boolean {
+        return ModelClass().someLibraryMethod()
+    }
+}
---
+++ kolibri-core/src/test/kotlin/com/ran/kolibri/core/CoreClassTest.kt
@@ -0,0 +1,11 @@
+package com.ran.kolibri.core
+
+import kotlin.test.Test
+import kotlin.test.assertTrue
+
+class CoreClassTest {
+    @Test fun testSomeLibraryMethod() {
+        val classUnderTest = CoreClass()
+        assertTrue(classUnderTest.someLibraryMethod(), "someLibraryMethod should return 'true'")
+    }
+}
