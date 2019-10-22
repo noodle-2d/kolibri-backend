db9720c07b83bb51b9f8a80429b003c70ea2ab24
Ilya Kuznetsov <et.anchoret@gmail.com>
2019-10-22 23:23:21 +0300

    Dependencies were refactored in gradle build files.

--- kolibri-idp/kolibri-idp-api/build.gradle.kts
+++ kolibri-idp/kolibri-idp-api/build.gradle.kts
@@ -1,11 +1,5 @@
 dependencies {
-    implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-
     implementation(project(":kolibri-idp:kolibri-idp-core"))
-
-    testImplementation("org.jetbrains.kotlin:kotlin-test")
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
 }
 
 application {
--- kolibri-idp/kolibri-idp-core/build.gradle.kts
+++ kolibri-idp/kolibri-idp-core/build.gradle.kts
@@ -1,9 +1,3 @@
 dependencies {
-    implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-
     implementation(project(":kolibri-core"))
-
-    testImplementation("org.jetbrains.kotlin:kotlin-test")
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
 }
